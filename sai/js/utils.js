10d1b4c373228944e511df8a2848631bfea4a24b
venkat-photon <venkataraman.kumar@photoninfotech.net>
2017-04-26 19:06:41 +0530

    Drag and Drop

--- sai/js/utils.js
+++ sai/js/utils.js
@@ -1,3 +1,7 @@
+var draggable = '';
+var draggableId = '';
+var count = 0;
+
 function changeWidth(minwidth, maxwidth){
     $('#containerBody').css('min-width', minwidth);
     $('#containerBody').css('max-width', maxwidth);
@@ -6,12 +10,12 @@ function initialize(){
 	var windowHeight = $(window).height();
 	var windowWidth = $(window).width();
 	$('#sideNav').height(windowHeight);
-	$('#containerBody').css('min-height',windowHeight);
+	$('#containerBody').css('min-height',windowHeight-64);
 	$(window).resize(function(){
 		var windowHeight = $(window).height();
 		var windowWidth = $(window).width();
 		$('#sideNav').height(windowHeight);
-		$('#sideNav').css('min-height',windowHeight);
+		$('#sideNav').css('min-height',windowHeight-64);
 	});
 }
 function toggleSideBar(){
@@ -23,13 +27,30 @@ function allowDrop(ev) {
 
 function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
+	$('<div id="temp"></div>').insertAfter(ev.target);
+	draggable = ev.target;
+	draggableId = ev.target.id;
 }
 
 function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+	$(document.getElementById(data)).appendTo('#containerBody');
+	$(draggable).clone().insertAfter('#temp');
+	$('#containerBody #'+ draggableId).attr('id' , 'template-'+count);
 	toggleSideBar();
 	var windowHeight = $(window).height();
-	$('.templates').height(windowHeight);
+	$('#containerBody .templates').height(windowHeight-64);
+	$('#temp').remove();
+	count++;
+	$('#containerBody .templates').click(function(){
+		$('#containerBody .templates').removeClass('active');
+		$(this).addClass('active');
+		$(this).find('.templateEditIcons .fa-trash-o').attr('onclick', 'javascript:deleteTemplate("'+ $(this).attr("id") +'");');
+		$(this).find('.templateEditIcons').removeClass('hide');
+	});
+}
+
+function deleteTemplate(id){
+	$('#'+id).remove();
 }
