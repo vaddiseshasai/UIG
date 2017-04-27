f26a4366c23ba1e50644e9aaa348141e68ff852e
Aravindhan DK <aravind1308@gmail.com>
2017-04-27 10:02:05 +0530

    Code changes

--- sai/js/utils.js
+++ sai/js/utils.js
@@ -54,3 +54,11 @@ function drop(ev) {
 function deleteTemplate(id){
 	$('#'+id).remove();
 }
+
+function previewCode(){
+	var opened = window.open("");
+	opened.document.write('<!DOCTYPE html><html><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="generator" content="Mobirise v3.12.1, mobirise.com"><meta name="viewport" content="width=device-width, initial-scale=1"><meta name="description" content=""><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic&amp;subset=latin"><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700"><link href="https://fonts.googleapis.com/css?family=Rouge+Script|Satisfy" rel="stylesheet"><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i"><link rel="stylesheet" href="assets/bootstrap-material-design-font/css/material.css"><link rel="stylesheet" href="assets/tether/tether.min.css"><link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css"><link rel="stylesheet" href="assets/dropdown/css/style.css"><link rel="stylesheet" href="assets/animate.css/animate.min.css"><link rel="stylesheet" href="assets/theme/css/style.css"><link rel="stylesheet" href="font-awesome/css/font-awesome.min.css"><link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css"><link rel="stylesheet" href="css/style.css" type="text/css"></head>');
+	opened.document.write('<body>'+ $('#containerBody').html());
+	opened.document.write('<section class="engine"><a rel="external" href="https://mobirise.com">Mobirise Website Builder</a></section><script src="assets/web/assets/jquery/jquery.min.js"></script><script src="js/utils.js"></script><script src="assets/tether/tether.min.js"></script><script src="assets/bootstrap/js/bootstrap.min.js"></script><script src="assets/smooth-scroll/smooth-scroll.js"></script><script src="assets/dropdown/js/script.min.js"></script><script src="assets/touch-swipe/jquery.touch-swipe.min.js"></script><script src="assets/viewport-checker/jquery.viewportchecker.js"></script><script src="assets/theme/js/script.js"></script><input name="animation" type="hidden">');
+	opened.document.write('</body></html>');
+}
