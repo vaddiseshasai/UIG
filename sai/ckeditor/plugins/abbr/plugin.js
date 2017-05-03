bdd224decb78bde9f79787805ac825147b07302b
SESHASAI VADDI <vaddi.seshasai@gmail.com>
2017-05-03 12:19:17 +0530

    add editor

---
+++ sai/ckeditor/plugins/abbr/plugin.js
@@ -0,0 +1,13 @@
+CKEDITOR.plugins.add( 'abbr', {
+    icons: 'abbr',
+    init: function( editor ) {
+        // Plugin logic goes here...
+		editor.addCommand( 'abbr', new CKEDITOR.dialogCommand( 'abbrDialog' ) );
+		editor.ui.addButton( 'Abbr', {
+			label: 'Insert Abbreviation',
+			command: 'abbr',
+			toolbar: 'insert'
+		});
+		CKEDITOR.dialog.add( 'abbrDialog', this.path + 'dialogs/abbr.js' );
+    }
+});
